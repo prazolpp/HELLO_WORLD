6dbe81e96ea350c8fd89d0219daf9bd00c8f9477
Prajwal Pyakurel <prazol.pp@gmail.com>
2021-03-07 12:16:20 -0800

    set react project structure

---
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+import './LandingPage.css';
+
+const LandingPage = ({}) => {
+
+    const getStarted = () => {
+    };
+
+    return (
+        <div className="LandingPage">
+            {/*Add thee landing page html here and modify components as needed */}
+        </div>
+    );
+};
+
+export default LandingPage;
