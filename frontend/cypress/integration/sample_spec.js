f7c07c9859b42fa6b20bcea1fb260a163e1e0917
Alan Rodriguez <alanrodriguez102347@gmail.con>
2021-05-18 15:59:52 -0700

    All Cypress Testing

--- frontend/cypress/integration/sample_spec.js
+++ frontend/cypress/integration/sample_spec.js
@@ -1,5 +1,98 @@
-describe('My First Test', function() {
-    it('Visits the kitchen sink', function(){
-       cy.visit('https://example.cypress.io')
+describe('Landing Page', function(){
+    it('Visits the landing page', function(){
+        cy.visit('http://localhost:3000/landingpage')
+
+        cy.contains('Home').click()
+        cy.url()
+            .should('include', 'http://localhost:3000/landingpage')
+
+        cy.contains('Analytics').click()
+        cy.url()
+            .should('include', 'http://localhost:3000/analytics')
+
+        cy.contains('Profile').click()
+        cy.url()
+            .should('include', 'http://localhost:3000/profile')
+
+        cy.contains('Learn more about Klout!').click()
+        cy.url().should('include', 'http://localhost:3000/app.html')
+        
     })
 })
+
+describe('Analytics Page', function(){
+    it('Visits the landing page', function(){
+        cy.visit('http://localhost:3000/analytics')
+
+        cy.contains('Home').click()
+        cy.url()
+            .should('include', 'http://localhost:3000/landingpage')
+
+        cy.contains('Analytics').click()
+        cy.url()
+            .should('include', 'http://localhost:3000/analytics')
+
+        cy.contains('Profile').click()
+        cy.url()
+            .should('include', 'http://localhost:3000/profile')
+
+        cy.contains('Learn more about Klout!').click()
+        cy.url().should('include', 'http://localhost:3000/app.html')
+        
+    })
+})
+
+describe('Profile Page', function(){
+    it('Visits the landing page', function(){
+        cy.visit('http://localhost:3000/profile')
+
+        cy.contains('Home').click()
+        cy.url()
+            .should('include', 'http://localhost:3000/landingpage')
+
+        cy.contains('Analytics').click()
+        cy.url()
+            .should('include', 'http://localhost:3000/analytics')
+
+        cy.contains('Profile').click()
+        cy.url()
+            .should('include', 'http://localhost:3000/profile')
+
+        cy.contains('Learn more about Klout!').click()
+        cy.url().should('include', 'http://localhost:3000/app.html')
+        
+    })
+})
+
+describe('Google SSO', function(){
+    it('Visits the landing page', function(){
+        cy.visit('http://localhost:3000/profile')
+
+        Cypress.Commands.add('login', (overrides = {}) => {
+            Cypress.log({
+              name: 'loginViaAuth0',
+            });
+          
+            const options = {
+              method: 'POST',
+              url: Cypress.env('auth_url'),
+              body: {
+                grant_type: 'password',
+                username: Cypress.env('auth_username'),
+                password: Cypress.env('auth_password'),
+                audience: Cypress.env('auth_audience'),
+                scope: 'openid profile email',
+                client_id: Cypress.env('auth_client_id'),
+                client_secret: Cypress.env('auth_client_secret'),
+              },
+            };
+            cy.request(options);
+          });
+        
+    })
+})
+
+
+
+
+
